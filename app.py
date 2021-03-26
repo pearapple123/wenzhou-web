03dfa5d767941ac8c4161cdda2777d2ceae37468
Cookie Dough <stickytoffeepuddingwithcaramel@gmail.com>
2021-03-26 16:53:11 +0000

    tried to have a single route instead

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
-from flask import Flask, render_template, url_for, request, redirect, session
-from form import SearchForm
-import json
-from flask_session import Session
-# from toipa import shentoipa
+from flask import Flask, render_template, url_for, redirect, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-SESSION_TYPE = 'redis'
-app.config.from_object(__name__)
-Session(app)
 app.config['SECRET_KEY'] = "bbe039da711aa33120ffe25823ae104d1daa0d8efc3e8b3ceb37a4819bf3f695"
 
-with open("sample.json", "r", encoding="utf-8") as f:
-    jsond = f.read()
-d = json.loads(jsond)
+
+class SearchForm(FlaskForm):
+    term = StringField(label=('Search for:'), validators=[DataRequired()])
+    submit = SubmitField(label=('Search'))
+
+
+d = {
+    "我": {
+        "eng": "I, me",
+        "pin": "ng4"
+    },
+    "你": {
+        "eng": "you",
+        "pin": "nyi4"
+    }
+}
 
 
 @app.route("/", methods=['GET', 'POST'])
 def search():
     form = SearchForm()
-    if form.is_submitted():
-        session['hanterm'] = request.form.get("term")
-        return redirect("results.html")
+    if request.method == "POST":
+        print(form.term.data)
+        hanterm = form.term.data
+        postdata = d[hanterm]
+        return render_template("results.html", title=hanterm, postdata=postdata)
     return render_template("index.html", title="Search", form=form)
-
-
-@app.route("/result")
-def results():
-    getdata = str(session.get("hanterm", None))
-    print("Getdata is " + getdata)
-    postdata = d[getdata]
-    return render_template("results.html", title=getdata, postdata=postdata)
 
 
 if __name__ == '__main__':
--- form.py
+++ form.py
@@ -1,6 +1,4 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+
 
 
 class SearchForm(FlaskForm):
